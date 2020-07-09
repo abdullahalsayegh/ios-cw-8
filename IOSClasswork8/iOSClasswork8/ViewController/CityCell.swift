42acb54282cb7e03d4813c88ef25efcc990fe18b
abdullah <Ab.alsayegh2004@gmail.com>
2020-07-09 03:52:29 +0300

    Done

---
+++ IOSClasswork8/iOSClasswork8/ViewController/CityCell.swift
@@ -0,0 +1,16 @@
+//
+//  CityCell.swift
+//  iOSClaswork8
+//
+//  Created by abdullah alsayegh on 09/07/2020.
+//  Copyright © 2020 Safeyah Coding. All rights reserved.
+//
+
+import UIKit
+
+class CityCell: UITableViewCell {
+    @IBOutlet weak var cityImageView: UIImageView!
+    @IBOutlet weak var cityNameLabel: UILabel!
+    }
+
+   
